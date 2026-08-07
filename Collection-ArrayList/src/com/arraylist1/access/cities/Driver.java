1e2d6756ad2291e32a8ed441bd21d49b2287eb4d
Krishnawasthi <km.awasthi.77@gmail.com>
2026-08-07 21:38:03 +0530

    arratlsur

---
+++ Collection-ArrayList/src/com/arraylist1/access/cities/Driver.java
@@ -0,0 +1,28 @@
+package com.arraylist1.access.cities;
+
+import java.util.ArrayList;
+
+public class Driver {
+
+	public static void main(String[] args) {
+		
+		ArrayList<String> cities = new ArrayList<String>(); //default capacity 10 ...increased by 50%.
+		
+		cities.add("BLR");
+		cities.add("CHE");
+		cities.add("HYD");
+		cities.add("AHM");
+		cities.add("DEL");
+		
+		
+		
+		cities.add(1, "KOL");
+		
+		cities.remove(1);
+		
+		System.out.println(cities);
+		
+	}
+	
+}
+		
